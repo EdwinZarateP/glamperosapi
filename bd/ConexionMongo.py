406aa659540b2890d483d4049bb0fc7e2ea77cb5
EdwinZarateP <emzp1994@gmail.com>
2024-11-22 16:19:17 -0500

    se crearon variables entorno en render

--- bd/ConexionMongo.py
+++ bd/ConexionMongo.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 
 ConexionMongo = MongoClient("mongodb+srv://glamperos:glamperos2025@glamperosapi.8gnlu.mongodb.net/?retryWrites=true&w=majority&appName=glamperosapi")                          
-                          
+    
+
                           
