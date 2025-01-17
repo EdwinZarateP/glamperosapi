05a75fd87e4b9b805328e169b8a31365859ca170
EdwinZarateP <emzp1994@gmail.com>
2025-01-17 16:26:11 -0500

    se usa plantilla

--- rutas/whatsapp.py
+++ rutas/whatsapp.py
@@ -11,23 +11,24 @@ ruta_whatsapp = APIRouter(
 )
 
 # Modelo de entrada para el mensaje
-class MensajeWhatsAppDirecto(BaseModel):
+class MensajeWhatsAppTemplate(BaseModel):
     numero: str
-    mensaje: str
+    parametros: dict
 
     """
-    Envía un mensaje de texto directo por WhatsApp.
+    Envía un mensaje utilizando una plantilla aprobada.
     - `numero`: Número de WhatsApp del destinatario.
-    - `mensaje`: El mensaje que quiere enviar.
+    - `parametros`: Diccionario con los valores para reemplazar las variables de la plantilla.
     """
 
-# Ruta para enviar mensaje por WhatsApp
-@ruta_whatsapp.post("/enviar-directo")
-async def enviar_mensaje_directo(mensaje: MensajeWhatsAppDirecto):
+# Ruta para enviar mensajes usando la plantilla aprobada
+@ruta_whatsapp.post("/enviar-plantilla")
+async def enviar_mensaje_plantilla(mensaje: MensajeWhatsAppTemplate):
     # Credenciales de Twilio desde las variables de entorno
     account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
     auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
     from_whatsapp = 'whatsapp:+573215658598'
+    template_sid = 'HX9fbee548a467e2255d2b1e2ed7d4dd2f'
 
     if not account_sid or not auth_token:
         raise HTTPException(status_code=500, detail="Twilio credentials not set in environment variables.")
@@ -35,11 +36,15 @@ async def enviar_mensaje_directo(mensaje: MensajeWhatsAppDirecto):
     client = Client(account_sid, auth_token)
 
     try:
-        # Envío del mensaje directo
+        # Envío del mensaje utilizando la plantilla
         mensaje_twilio = client.messages.create(
             from_=from_whatsapp,
-            body=mensaje.mensaje,
-            to=f'whatsapp:{mensaje.numero}'
+            to=f'whatsapp:{mensaje.numero}',
+            template={
+                'name': 'confirmacion',
+                'parameters': mensaje.parametros
+            },
+            content_sid=template_sid
         )
         return {"status": "success", "message_sid": mensaje_twilio.sid}
     except Exception as e:
